76f007d9af9f825cfae7ea3941e476c2786a31e4
Xopar <xoparr@gmail.com>
2023-12-12 15:28:12 -0600

    Fix errors onClick

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ const Player = ({playerInfo, onClick}) => {
   return (
     <div
       className={`${styles.playerBox} ${playerInfo.class}`}
-      onClick={(e) => onClick(playerInfo) ?? undefined}
+      onClick={onClick !== undefined ? (e) => onClick(playerInfo) : undefined }
     >
       {playerInfo.name}
     </div>
