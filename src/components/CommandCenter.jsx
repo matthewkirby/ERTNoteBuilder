777efe3006944c388e7a955952f0fe673b3b3181
Xopar <xoparr@gmail.com>
2023-12-16 14:51:16 -0600

    Small style changes to cc buttons

--- src/components/CommandCenter.jsx
+++ src/components/CommandCenter.jsx
@@ -1,21 +1,23 @@
 import styles from "../css/CommandCenter.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faClone } from '@fortawesome/free-solid-svg-icons';
 
 const CommandCenter = ({ insertNewRow, exportNote, tooltipVisible }) => {
 
   // Put button with up/down halves and arrow unicode things to specify if inserting line above or below selection
 
+  // Triple button with insertLeft/Replace/insertRight buttons
+
   return (
     <div className={styles.commandCenter}>
-      <FontAwesomeIcon className={[styles.commandIcon, styles.delete].join(' ')} icon={faCaretDown} />
+      <FontAwesomeIcon className={styles.buttonDelete} icon={faCaretDown} />
       <div className={styles.command} onClick={() => insertNewRow()}>Insert New Row</div>
       <div className={styles.command}>Insert Text Field</div>
       <div
-        className={[styles.command, styles.flushRight, styles.tooltipBase].join(' ')}
+        className={[styles.command, styles.flushRight, styles.tooltipBase, styles.buttonCopy].join(' ')}
         onClick={() => exportNote()}
       >
-        Copy to Clipboard
+        <FontAwesomeIcon icon={faClone} size="lg" />
         <div className={[styles.tooltipText, tooltipVisible ? "visible" : "hidden"].join(' ')}>Copied!</div>
       </div>
     </div>
