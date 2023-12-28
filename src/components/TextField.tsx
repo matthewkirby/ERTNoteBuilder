8a298ed8804234f98c155108a032b91b426eb9f8
Xopar <xoparr@gmail.com>
2023-12-28 16:42:02 -0600

    Converted text fields to typescript

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,25 +1,28 @@
 import styles from "css/TextField.module.css";
+import React from "react";
 import { getTextWidth } from "utils/formatting";
 
 // When I click to add a new row at the top, a text field is getting selected
 // Handle max width to prevent clipping out of app
 
-const TextField = ({content, onClick, onChange}) => {
+ interface TextFieldProps {
+    content: string;
+    onClick: () => void;
+    onChange: (e: string) => void;
+ }
 
-  const handleClick = (e) => { e.stopPropagation(); onClick(); };
+const TextField: React.FC<TextFieldProps> = ({content, onClick, onChange}) => {
   const fieldWidth = getTextWidth(content, "19.2px segoe ui semibold");
-
   return (
     <input
       className={[styles.textBox].join(' ')}
       value={content}
       style={{width: fieldWidth}}
       autoFocus
-      onClick={(e) => handleClick(e)}
+      onClick={(e) => { e.stopPropagation(); onClick(); }}
       onChange={(e) => onChange(e.target.value)}
       onFocus={(e) => e.target.select()}
     />);
-
 }
 
 export default TextField;
